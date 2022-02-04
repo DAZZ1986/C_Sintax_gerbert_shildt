191945d492e4fa03b1379e2f04a050edce7143bb
Murtazov Artur <art.murtazov@gmail.com>
2022-02-04 13:16:50 +0300

    upsate from home on master branch

--- ExternTest/Player.c
+++ ExternTest/Player.c
@@ -1,8 +1,11 @@
 #include <stdio.h>        // подключаем заголовочный файл stdio.h
 #include <locale.h>
 
-//глобальные переменные функции
-int g_Age = 15;
+
+//глобальные переменные и функция
+int g_Age = 5;
+int g_Speed;
+
 int g_ShowBalance()
 {
 	g_Age += 20;
